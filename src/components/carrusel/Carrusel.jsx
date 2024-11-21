060ce266810a7824224a94538b3c472743547453
Nelsper <nelsper@gmail.com>
2024-11-21 10:05:39 -0500

    install embla

---
+++ src/components/carrusel/Carrusel.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./css/carrusel.css";
+
+const Carrusel = () => {
+
+    
+  return (
+    <>
+        <h1>Your Welcome</h1>
+    </>
+  )
+};
+
+export default Carrusel;
