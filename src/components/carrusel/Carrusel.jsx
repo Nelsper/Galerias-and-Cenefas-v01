0f4296be2a5da24eb4967b10cec7bf65e3b68d1e
Nelsper <nelsper@gmail.com>
2024-11-23 13:28:30 -0500

    carrousel ok

--- src/components/carrusel/Carrusel.jsx
+++ src/components/carrusel/Carrusel.jsx
@@ -1,14 +1,59 @@
-import React from "react";
+import { useCallback, useEffect } from "react";
 import "./css/carrusel.css";
+import useEmblaCarousel from "embla-carousel-react";
+import slides from "../../data";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCircleChevronUp,
+  faCircleChevronDown,
+} from "@fortawesome/free-solid-svg-icons";
 
 const Carrusel = () => {
+  const [emblaRef, emblaApi] = useEmblaCarousel({
+    axis: "y",
+    loop: true,
+  });
 
-    
+  const scrollPrev = useCallback(() => {
+    if (emblaApi) emblaApi.scrollPrev();
+  }, [emblaApi]);
+
+  const scrollNext = useCallback(() => {
+    if (emblaApi) emblaApi.scrollNext();
+  }, [emblaApi]);
+
+  useEffect(() => {
+    if (emblaApi) {
+      emblaApi.reInit(); // Asegura que el carrusel se inicializa correctamente
+    }
+  }, [emblaApi]);
+
   return (
     <>
-        <h1>Your Welcome</h1>
+      <h1>Your Welcome!</h1>
+      <h1>Carrusel Vertical</h1>
+      <div className="embla">
+        <div className="embla__viewport" ref={emblaRef}>
+          <div className="embla__container">
+            {slides.map((slide) => (
+              <div className="embla__slide" key={slide.id}>
+                <img src={slide.img} alt="" />
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+
+      <div className="btns">
+                <button className="embla__button embla__button--prev" onClick={scrollPrev}>
+                    <FontAwesomeIcon icon={faCircleChevronDown} size='2xl'/>
+                 </button>
+                <button className="embla__button embla__button--next" onClick={scrollNext}>
+                    <FontAwesomeIcon icon={faCircleChevronUp} size='2xl'/>
+                </button>
+            </div>
     </>
-  )
+  );
 };
 
 export default Carrusel;
---
+++ src/data.js
@@ -0,0 +1,64 @@
+const slides = [
+    {
+        id: '1',
+        img: '01.jpg',
+        descrip: ''
+    },
+    {
+        id: '2',
+        img: '02.jpg',
+        descrip: ''
+    },
+    {
+        id: '3',
+        img: '03.jpg',
+        descrip: ''
+    },
+    {
+        id: '4',
+        img: '04.jpg',
+        descrip: ''
+    },
+    {
+        id: '5',
+        img: '05.jpg',
+        descrip: ''
+    },
+    {
+        id: '6',
+        img: '06.jpg',
+        descrip: ''
+    },
+    {
+        id: '7',
+        img: '07.jpg',
+        descrip: ''
+    },
+    {
+        id: '8',
+        img: '08.jpg',
+        descrip: ''
+    },
+    {
+        id: '9',
+        img: '09.jpg',
+        descrip: ''
+    },
+    {
+        id: '10',
+        img: '10.jpg',
+        descrip: ''
+    },
+    {
+        id: '11',
+        img: '11.jpg',
+        descrip: ''
+    },
+    {
+        id: '12',
+        img: '12.jpg',
+        descrip: ''
+    }
+]
+
+export default slides;
